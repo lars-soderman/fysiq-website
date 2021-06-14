f27ed7135d95774aeedd4e598d408f2e1dbd068d
Lars Söderman <lars.soderman@gmail.com>
2021-06-14 18:31:52 +0200

    domain

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3 style={{ position: 'absolute', top: '50%', left: '50%',   transform: 'translate(-50%, -50%)'
+ }}>Guten tag world</h3>
     </div>
   );
 }
